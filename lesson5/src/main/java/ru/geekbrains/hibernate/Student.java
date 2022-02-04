a7fb11da0e206cbcd361513f81878bc8d37e4ca6
Elgk <lena.elena-kuznetzova2016@yandex.ru>
2022-02-04 20:46:51 +0400

    Homework 5

---
+++ lesson5/src/main/java/ru/geekbrains/hibernate/Student.java
@@ -0,0 +1,56 @@
+package ru.geekbrains.hibernate;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "student")
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "mark")
+    private int mark;
+
+    public Student(){
+    }
+
+    public Student(Long id, String name, int mark) {
+        this.name = name;
+        this.mark = mark;
+        this.id = id;
+    }
+
+    public int getMark() {
+        return mark;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setMark(int mark) {
+        this.mark = mark;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Student: [ id = %d, name = %s, mark = %d] ", id, name, mark);
+    }
+}
---
+++ lesson5/src/main/java/ru/geekbrains/hibernate/StudentDao.java
@@ -0,0 +1,71 @@
+package ru.geekbrains.hibernate;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
+import java.util.List;
+
+
+public class StudentDao {
+    private Session currentSession;
+    private Transaction currentTransaction;
+    private final SessionFactory sessionFactory;
+
+    public StudentDao(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
+    public Session openCurrentSession() {
+        currentSession = sessionFactory.getCurrentSession();
+        return currentSession;
+    }
+
+    public Session openCurrentSessionwithTransaction(){
+        currentSession = sessionFactory.getCurrentSession();
+        currentTransaction = currentSession.beginTransaction();
+        return currentSession;
+    }
+
+    public void closeSession(){
+        currentSession.close();
+    }
+
+    public void closeSessionWitnTransaction(){
+        currentTransaction.commit();
+        currentSession.close();
+    }
+
+    public void persist(Student student){
+        openCurrentSessionwithTransaction();
+        currentSession.save(student);
+        closeSessionWitnTransaction();
+    }
+
+    public void update(Student student){
+        openCurrentSessionwithTransaction();
+        currentSession.update(student);
+        closeSessionWitnTransaction();
+    }
+
+    public void delete(Student student){
+        openCurrentSessionwithTransaction();
+        currentSession.delete(student);
+        closeSessionWitnTransaction();
+    }
+
+    public Student findById(Long id){
+        openCurrentSessionwithTransaction();
+        Student student = currentSession.get(Student.class, id);
+        closeSessionWitnTransaction();
+        return  student;
+    }
+
+    public List<Student> findAll(){
+       openCurrentSessionwithTransaction();
+        List<Student> list = currentSession.createQuery("select a from Student  a", Student.class).getResultList();
+        closeSessionWitnTransaction();
+        return  list;
+    }
+
+}
